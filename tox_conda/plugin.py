c39df5bfbedf9ed7039c0208609d7805bc93ef36
Tiemen Schuijbroek <t.j.l.schuijbroek@outlook.com>
2018-06-06 01:24:34 +0200

    More elegant check for conda on path
    More elegant conda on path check

--- tox_conda/plugin.py
+++ tox_conda/plugin.py
@@ -6,14 +6,14 @@ import sys
 import pluggy
 
 hookimpl = pluggy.HookimplMarker("tox")
-log = logging.getLogger('conda')
-
+log = logging.getLogger("conda")
 
 on_win = bool(sys.platform == "win32")
 
 
 @hookimpl
 def tox_testenv_create(venv, action):
+    """Perform creation action for this venv."""
     deps = venv.envconfig.deps
     conda_deps, pip_deps = split_conda_deps(deps)
     venv.envconfig.deps = pip_deps
@@ -27,7 +27,7 @@ def tox_testenv_create(venv, action):
     python_version = basepython[6:]
     env_location = str(venv.path)
     args = [
-        '%s' % os.environ["CONDA_EXE"],
+        "%s" % os.environ["CONDA_EXE"],
         "install",
         "--mkdir",
         "-y",
@@ -45,45 +45,8 @@ def tox_testenv_create(venv, action):
     return True if result is None else result
 
 
-@hookimpl
-def tox_runtest_pre(venv):
-    env_var_map = get_activated_env_vars(venv)
-    os.environ.clear()
-    os.environ.update(env_var_map)
-
-
-def get_activated_env_vars(venv):
-    env_location = str(venv.path)
-    cmd_builder = []
-    if on_win:
-        conda_bat = os.environ["CONDA_BAT"]
-        cmd_builder += [
-            "CALL \"{0}\" activate \"{1}\"".format(conda_bat, env_location),
-            "&&",
-            "%CONDA_PYTHON_EXE% -c \"import os, json; print(json.dumps(dict(os.environ)))\"",
-        ]
-    else:
-        conda_exe = os.environ["CONDA_EXE"]
-        cmd_builder += [
-            "sh -c \'"
-            "eval \"$(\"{0}\" shell.posix hook)\"".format(conda_exe),
-            "&&",
-            "conda activate {0}".format(env_location),
-            "&&",
-            "\"$CONDA_PYTHON_EXE\" -c \"import os, json; print(json.dumps(dict(os.environ)))\"",
-            "\'",
-        ]
-
-    cmd = " ".join(cmd_builder)
-    env = venv._getenv(testcommand=False)
-    action = venv.session.newaction(venv, "activate")
-    with action:
-        result = action.popen([cmd], env=env, redirect=False, returnout=True, shell=True)
-    env_var_map = json.loads(result)
-    return env_var_map
-
-
 def split_conda_deps(deps):
+    """Split dependencies between conda and pip."""
     conda_deps = []
     pip_deps = []
     for dep in deps:
@@ -98,3 +61,58 @@ def split_conda_deps(deps):
     return conda_deps, pip_deps
 
 
+@hookimpl
+def tox_runtest_pre(venv):
+    env_var_map = get_activated_env_vars(venv)
+    os.environ.clear()
+    os.environ.update(env_var_map)
+
+
+def get_activated_env_vars(venv):
+    env_location = str(venv.path)
+    cmd_builder = []
+    conda = None
+    if on_win:
+        exes = ["conda.exe", "conda.bat"]
+        for exe in exes:
+            if is_on_path(exe):
+                conda = exe
+                cmd_builder += [
+                    'CALL "{0}" activate "{1}"'.format(conda, env_location),
+                    "&&",
+                    '%CONDA_PYTHON_EXE% -c "import os, json; print(json.dumps(dict(os.environ)))"',
+                ]
+                break
+    else:
+        exes = ["conda", "conda.sh"]  # TODO: Does it need conda.sh?
+        for exe in exes:
+            if is_on_path(exe):
+                conda = exe
+                cmd_builder += [
+                    "sh -c '" 'eval "$("{0}" shell.posix hook)"'.format(conda),
+                    "&&",
+                    "conda activate {0}".format(env_location),
+                    "&&",
+                    '"$CONDA_PYTHON_EXE" -c "import os, json; print(json.dumps(dict(os.environ)))"',
+                    "'",
+                ]
+    if not conda:
+        raise RuntimeError("Unable to find conda on path.")
+
+    cmd = " ".join(cmd_builder)
+    env = venv._getenv(testcommand=False)
+    action = venv.session.newaction(venv, "activate")
+    with action:
+        result = action.popen(
+            [cmd], env=env, redirect=False, returnout=True, shell=True
+        )
+    env_var_map = json.loads(result)
+    return env_var_map
+
+
+def is_on_path(cmd):
+    """Check whether a command or executable is on path."""
+    return any(
+        os.access(os.path.join(path, cmd), os.X_OK)
+        for path in os.environ["PATH"].split(os.pathsep)
+    )
